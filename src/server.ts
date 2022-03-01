9dd53f9458d5be26dd8a83023b4d46423f353d07
Victor Amorim <victorvmrgamer@gmail.com>
2022-02-28 21:31:48 -0300

    feat: add app.use function

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
-const app = express();
-const port = process.env.PORT;
 
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
