b5d10725b5ce577fcc1707112a0be11974b5e6b9
Victor Amorim <victorvmrgamer@gmail.com>
2022-03-01 11:49:00 -0300

    feat: use categoriesRoute on app

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 
 import "./database";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
+app.use("/categories", categoriesRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
