395b5ae7ac5e94580f006f129eb52eb2b4d30a84
Victor Amorim <victorvmrgamer@gmail.com>
2022-03-02 18:37:14 -0300

    feat: call CategoriesRepository create metho

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,22 @@
 import { Router } from "express";
 
 import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category: Category = new Category();
-
-  Object.assign(category, {
+  categoriesRepository.create({
     name,
     description,
-    created_at: new Date(),
   });
 
-  categories.push(category);
-
-  return response.status(201).send({ category });
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
