b3404c01de82caf9d1dedea68c3af47c035f8f8c
Victor Amorim <victorvmrgamer@gmail.com>
2022-03-02 18:57:24 -0300

    feat: return an error if name is already in use

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -11,6 +11,12 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!" });
+  }
+
   categoriesRepository.create({
     name,
     description,
