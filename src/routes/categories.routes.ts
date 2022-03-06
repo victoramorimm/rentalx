213039bbce69e256c26a32b8933f3a5dd60643b5
Victor Amorim <victorvmrgamer@gmail.com>
2022-03-05 23:10:00 -0300

    refactor: call createCategoryController

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { Category } from "../modules/cars/model/Category";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -10,16 +10,7 @@ const categoriesRepository = new CategoriesRepository();
 const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({
-    name,
-    description,
-  });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
