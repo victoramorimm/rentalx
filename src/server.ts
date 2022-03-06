a2e65170064ebb71d958ad85227c2efce2377a41
Victor Amorim <victorvmrgamer@gmail.com>
2022-03-05 22:52:24 -0300

    feat: add specificationsRoutes to app

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,16 @@ import dotenv from "dotenv";
 
 import "./database";
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
