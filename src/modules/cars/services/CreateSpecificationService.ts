dc9b1f7b558c1f0119f9c32872c566146217a0b7
Victor Amorim <victorvmrgamer@gmail.com>
2022-03-05 22:50:58 -0300

    feat: create CreateSpecificationService service

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,27 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("a");
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+
+    console.log(this.specificationsRepository);
   }
 }
 
