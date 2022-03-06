efe9432679f7ad55e4ff67402a3a6f8147e8e777
Victor Amorim <victorvmrgamer@gmail.com>
2022-03-06 10:12:04 -0300

    feat: use all routes from router

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 
 import "./database";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
